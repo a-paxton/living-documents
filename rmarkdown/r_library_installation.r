a45be8735052695d2f49b1b3dc1c609ff68e787b
Alexandra Paxton <paxton.alexandra@gmail.com>
2018-04-05 17:48:18 -0700

    Keep working on R markdown examples

---
+++ rmarkdown/r_library_installation.r
@@ -0,0 +1,19 @@
+# In this R script, we'll install any packages we might need
+# if they're not installed already.
+
+# Written by: A. Paxton (University of California, Berkeley)
+# Date last modified: 05 April 2018
+
+# list of required packages
+required_packages = c(
+  'ggplot2'
+)
+
+# install missing packages (adapted from <http://stackoverflow.com/a/4090208>)
+missing_packages = required_packages[!(required_packages %in% installed.packages()[,"Package"])]
+if (length(missing_packages) > 0) {
+  install.packages(missing_packages)
+}
+
+# load required packages
+invisible(lapply(required_packages, require, character.only = TRUE))
